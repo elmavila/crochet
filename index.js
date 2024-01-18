73395be680188077d35351875c579a107c93cb4b
Elma <vilaelma1@gmail.com>
2024-01-18 09:26:00 +0100

    new

--- index.js
+++ index.js
@@ -14,8 +14,14 @@ function getPattern() {
       "Scissors",
       "Needle",
     ],
-    "Comfortable fingergloves suitable for any hand size",
-    ["buble", "buble"]
+    "Comfortable fingergloves suitable for any hand size with a simple pattern for even beginners. You may come across some new stitches, but everything is displayd above."
+    ,
+    [
+      "Slip knot",
+      "Chain",
+      "Single Crochet",
+      "Back Loop Single Crochet",
+    ]
   );
 }
 
@@ -23,10 +29,17 @@ const pattern = getPattern();
 
 const materialsList = document.getElementById("materials");
 
-pattern.materials.forEach((m) => {
+pattern.materials.forEach((material) => {
   const li = document.createElement("li");
-  li.innerText = m;
+  li.innerText = material;
   materialsList.appendChild(li);
-  console.log(m);
+  console.log(material);
   console.log(li);
 });
+
+const introText = document.getElementById("intro");
+
+introText.innerText = pattern.intro;
+
+const stitchesList = document.getElementById("stitches");
+
